6665e5f0fb0b427514c8b522623a8bba1b6b9be5
Ravi Singh <singhravi4v@gmail.com>
2022-10-12 14:53:33 +0530

    feat:setup routings

--- src/app/company/company-routing.module.ts
+++ src/app/company/company-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompanyFormComponent } from './company-form/company-form.component';
 import { CompanyComponent } from './company.component';
 
-const routes: Routes = [{ path: '', component: CompanyComponent }];
+const routes: Routes = [{
+  path: '', component: CompanyComponent, children: [
+    { path: 'add', component: CompanyFormComponent },
+    { path: 'edit/:id', component: CompanyFormComponent },
+
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
