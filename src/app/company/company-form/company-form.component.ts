61f471693538763238ae4bd2d4a809a67f4543a5
Ravi Singh <singhravi4v@gmail.com>
2022-10-13 12:43:13 +0530

    feat:form validation done

--- src/app/company/company-form/company-form.component.ts
+++ src/app/company/company-form/company-form.component.ts
@@ -8,19 +8,30 @@ import { company } from '../company.model';
   styleUrls: ['./company-form.component.scss']
 })
 export class CompanyFormComponent implements OnInit {
+
   public companyData: company[]
   public companyForm: FormGroup;
+  public isSubmitted: boolean
   constructor(private formb: FormBuilder) {
     this.companyForm = formb.group({
       name: ['', [Validators.required]],
       description: ['', [Validators.required]],
       tags: ['', [Validators.required]]
     })
+    this.isSubmitted = false
     this.companyData = []
 
   }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.isSubmitted = true
+
+  }
+  onReset() {
+
   }
 
 }
